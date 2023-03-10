9f16da6ac46665609217bcf526fdd14ae6cef5b5
B.S. Randimal Perera <randimalperera62@gmail.com>
2023-03-10 19:54:45 +0530

    dssds

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ app.post('/handleJSON', function(req, res) {
 
 
 //Deploying the listener
-const port = process.env.port || 0;
+const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Express server listening on port
 ${port}`));
